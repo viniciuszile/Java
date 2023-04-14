15e5d13d4205db8b2eb42252ed156d085a6d1493
viniciuszile <vinizile@gmail.com>
2023-04-14 13:24:52 -0300

    adicionando a pasta jogos

---
+++ Jogos/src/main/java/Jogos/Jogo_Da_Velha.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Jogos;
+
+/**
+ *
+ * @author Zile
+ */
+public class Jogo_Da_Velha {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10;) {
+            System.out.println("Jogo da velha");
+        }
+
+    }
+}
